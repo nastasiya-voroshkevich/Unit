949ea5bb78d9345bfb44a05808ec58f264d9d44c
Nastya <the_sun.89@mail.ru>
2021-01-18 01:54:41 +0300

    first commit

---
+++ docs/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        navSpeed: 1000,
+        nav: true,
+        dots: false,
+        loop: true
+       // navText: ['Назад','Вперед']
+    });
+  });
